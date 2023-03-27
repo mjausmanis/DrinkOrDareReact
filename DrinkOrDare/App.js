0a1a24c3d953e0f19c46348b8a2f9794d3d07bf5
markusvilcans <markusvilcans@gmail.com>
2023-03-27 21:44:11 +0300

    splashscreen

--- DrinkOrDare/App.js
+++ DrinkOrDare/App.js
@@ -8,17 +8,36 @@ import CustomQuestions from "./Screens/CustomQuestions";
 import GameScreen from "./Screens/GameScreen";
 import Scoreboard from "./Screens/Scoreboard";
 import { lightTheme, darkTheme } from "./Theme/Themes";
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Audio } from "expo-av";
+import {View, StyleSheet, Text, Image} from "react-native";
+
+const StartupScreen = ({ navigation }) => {
+  useEffect(() => {
+    async function playSound() {
+      const { sound } = await Audio.Sound.createAsync(
+        require("./assets/beerburp.mp3")
+      );
+      await sound.playAsync();
+      setTimeout(() => {
+        navigation.navigate("MainMenu");
+      }, 2000);
+    }
+    playSound();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/Title_light.png')} style={styles.image} resizeMode="contain"/>
+    </View>
+  );
+};
 
 const Stack = createNativeStackNavigator();
-
 export const ThemeContext = React.createContext();
-
 export default function App() {  
   const [theme, setTheme] = useState('Light');
   const themeData = { theme, setTheme };
-
   const [sound, setSound] = useState();
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(
@@ -39,8 +58,6 @@ export default function App() {
     };
   }, []);
 
-
-
   return (
     <ThemeContext.Provider value={themeData}>
       <NavigationContainer theme={theme == 'Light' ? lightTheme : darkTheme}>
@@ -48,6 +65,7 @@ export default function App() {
           screenOptions={{
             headerShown: false
         }}>
+          <Stack.Screen name="StartupScreen" component={StartupScreen} />
           <Stack.Screen name="MainMenu" component={MainMenu} />
           <Stack.Screen name="Options">
         {(props) => <Options {...props} sound={sound} stopSound={stopSound} />}
@@ -62,3 +80,14 @@ export default function App() {
     </ThemeContext.Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: "center", 
+    justifyContent: "center"
+  },
+  image: {
+    maxWidth: 300
+  }
+});
